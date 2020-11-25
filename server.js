a257e901e12a12e82028c2f33ef1678c82aae8aa
Ryan Zelazny <RyanZelazny@gmail.com>
2020-11-25 15:16:53 -0500

    initial post set up

--- server.js
+++ server.js
@@ -20,6 +20,21 @@ app.get("/api/notes", function(req, res) {
     res.json(apiData);
 });
 
+app.post("/api/notes", function(req, res) {
+
+    var newNote = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newNote);
+
+    db.push(newNote);
+
+    res.json(newNote);
+});
+
   // If no matching route is found default to home
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
